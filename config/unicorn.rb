d887f8b31234e299f77b8c32a36de261b3740309
danigb <danigb@gmail.com>
2012-04-19 18:48:33 +0200

    Unicorn path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/blog/current"
+root = "/home/deployer/apps/Masqueunacasa/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.blog.sock"
-worker_processes 2
+worker_processes 1
 timeout 30
