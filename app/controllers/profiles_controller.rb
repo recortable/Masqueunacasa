39ec96c7be8fbc7bd2ecb87eddf4906cad3f244c
danigb <danigb@gmail.com>
2012-05-02 00:33:03 +0200

    Preparando para testear subdominios

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+  before_filter :load_subdomain_group
+
+  def edit
+
+  end
+end
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -57,7 +57,7 @@ SimpleForm.setup do |config|
   end
 
   config.wrappers :checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
-    b.use :tag => 'div', :class => 'controls' do |ba|
+    b.wrapper :tag => 'div', :class => 'controls' do |ba|
       ba.use :label_input
     end
   end
