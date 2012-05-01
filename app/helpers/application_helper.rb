e065a24e60985fca6a61e90fd107a5273c524ec3
danigb <danigb@gmail.com>
2012-05-01 17:41:37 +0200

    Preparando la segunda pantalla

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     content_for(:title) { title }
     content_tag(tag, title)
   end
+
+  def controller_class
+    controller.class.name[0..-11].downcase.gsub(/:/, '-')
+  end
 end
