e02ae0a33bef8b5f45e79268e1d652ca6d1c1aa6
danigb <danigb@gmail.com>
2012-05-02 15:26:58 +0200

    Copy cloudinary.yml file

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/cloudinary.yml #{release_path}/config/cloudinary.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
