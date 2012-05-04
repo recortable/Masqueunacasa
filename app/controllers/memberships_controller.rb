9b9fadeaeda41af1da6106face71f28175b07b70
danigb <danigb@gmail.com>
2012-05-04 11:11:19 +0200

    Se pueden añadir participantes a un grupo

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,9 +1,11 @@
+# encoding: utf-8
 class MembershipsController < ApplicationController
   respond_to :html
   before_filter :load_subdomain_group
 
   expose(:users) { User.all }
   expose(:memberships) { current_group.memberships }
+  expose(:membership)
 
   def index
     respond_with memberships
@@ -11,5 +13,18 @@ class MembershipsController < ApplicationController
 
   def new
   end
-  
+
+  def create
+    user = User.find params[:u]
+    if current_group.member?(user)
+      redirect_to memberships_path, notice: 'Ya pertenecía a éste grupo'
+    else
+      membership.user = user
+      membership.group = current_group
+      membership.state = 'member'
+      flash[:notice] = 'Añadido!' if membership.save
+      respond_with membership, location: memberships_path
+    end
+  end
+
 end
