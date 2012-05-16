6ea54579f8e6c2540034082fac00dafe6cada57b
Pau Compte <pausandalio@gmail.com>
2012-05-16 22:16:48 +0200

    Using CKEditor instead of bootstrap-wysihtml5-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require bootstrap-wysihtml5-all
+//= require ckeditor/init
 //= require_self
 
-jQuery(function() {
-  $('textarea.rte').wysihtml5();
-});
+CKEDITOR.replaceClass = 'rte';
 
