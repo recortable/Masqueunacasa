d88c7aea6dc5024dc55d7589fdaff237c8570b83
Pau Compte <pausandalio@gmail.com>
2012-06-05 04:38:23 +0200

    Mapa en el formulario de perfil de grupo para entrar posición

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,12 @@
 $(function(){
-  initialize(null);
 
+  /* 
+   * Se llama dos veces la función initialize(), una al cargar la página
+   * i otra de nuevo si el usuario acepta compartir su ubicación.
+   * Un bug en firefox hace que no funcione 
+   * getCurrentPosition(onSuccessFunction, onErrorFunction)
+   */
+  initialize(null);
   navigator.geolocation.getCurrentPosition(initialize);
 
 });
@@ -9,8 +15,11 @@ function with_localization(position) {
   initialize(lat, lng, zoom);
 }
 
+var map;
+var marker;
+
 function initialize(position) {
-  $('#map_canvas').css({'width': '90%', 'height': '200px'});
+  $('#group_form_map').css({'width': '100%', 'height': '275px'});
   if (position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
@@ -23,8 +32,25 @@ function initialize(position) {
   var myOptions = {
     center: new google.maps.LatLng(lat, lng),
     zoom: zm,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP, 
+    disableDoubleClickZoom: true
   };
-  var map = new google.maps.Map(document.getElementById("map_canvas"),
+
+  map = new google.maps.Map(document.getElementById("group_form_map"),
             myOptions);
+  marker = new google.maps.Marker({
+    map: map
+  });
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    setLocation(event.latLng);
+  });
+}
+
+function setLocation(location) {
+  marker.setPosition(location);
+
+  var position = location.toUrlValue().split(",");
+  $('#group_latitude').val(position[0])
+  $('#group_longitude').val(position[1])
 }
