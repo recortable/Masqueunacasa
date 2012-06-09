631801892848ed39b273e2ea3e63b3839136977d
danigb <danigb@gmail.com>
2012-06-09 20:34:08 +0200

    Con un poco de suerte, closes #23

--- config/initializers/premailer.rb
+++ config/initializers/premailer.rb
@@ -3,4 +3,13 @@ if defined?(PremailerRails)
     preserve_styles: true,
     remove_ids: true
   }
+
+  module PremailerRails
+    module CSSHelper
+      def self.assets_enabled?
+        false
+      end
+    end
+  end
 end
+
