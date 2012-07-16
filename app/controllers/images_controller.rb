0558e109da691c5b82882ff028e1ca9ee120c0a2
Pau <pausandalio@gmail.com>
2012-07-16 20:28:53 +0200

    Se puede borrar y editar imágenes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,20 +5,45 @@ class ImagesController < ApplicationController
   expose(:image)
 
   def index
-    
+    authorize! :update, document
   end
 
   def new
-    render action: 'index'
+    
+  end
+
+  def edit
+    
   end
 
   def create
+    authorize! :update, document
     image.user = current_user
-    flash[:notice] = "imagen cargada" if image.save
+    flash[:notice] = "Imagen cargada!" if image.save
+    respond_with image, location: [document, :images]
+  end
+
+  def update
+    authorize! :update, document
+    flash[:notice] = "Imagen actualizada!" if image.save
     respond_with image, location: [document, :images]
   end
 
   def destroy
-    
+    authorize! :update, document
+    flash[:notice] = "Imagen borrada!" if image.destroy
+    respond_with image, location: [document, :images]
+  end
+
+  def up
+    s = Section.find params[:id]
+    s.move_higher
+    respond_with s, location: document_location(s)
+  end
+
+  def down
+    s = Section.find params[:id]
+    s.move_lower
+    respond_with s, location: document_location(s)
   end
 end
