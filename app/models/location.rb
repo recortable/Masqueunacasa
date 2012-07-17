8bd6c863d0b586290e6db100ccede2fdb9c42636
Pau <pausandalio@gmail.com>
2012-07-17 15:00:23 +0200

    Una localización sin título toma el título de su recurso asociado

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,12 +3,19 @@ class Location < ActiveRecord::Base
   attr_accessible :resource_id, :resource_type, :user_id
   attr_accessible :title, :latitude, :longitude
 
+  before_save :set_title
+
   belongs_to :user
   belongs_to :resource, polymorphic: true
 
-  validates :title, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
 
   scope :of_experiencies, where(resource_type: 'Expeiencie')
+
+  private
+
+    def set_title
+      self.title = self.resource.title if self.title.blank?
+    end
 end
