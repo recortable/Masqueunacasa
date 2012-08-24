22a64d78e3599e00768e273d9023af470bbfff8d
danigb <danigb@gmail.com>
2012-08-24 13:16:09 +0200

    Assets recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ load "config/recipes/postgresql"
 load "config/recipes/postgresql_backup"
 load "config/recipes/rbenv"
 load "config/recipes/check"
+load "config/recipes/assets"
 
 server "176.58.98.122", :web, :app, :db, primary: true
 
@@ -52,28 +53,5 @@ namespace :deploy do
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
-
-  namespace :assets do
-    desc <<-DESC
-      Run the asset precompilation rake task. You can specify the full path \
-      to the rake executable by setting the rake variable. You can also \
-      specify additional environment variables to pass to rake via the \
-      asset_env variable. The defaults are:
-
-        set :rake,      "rake"
-        set :rails_env, "production"
-        set :asset_env, "RAILS_GROUPS=assets"
-        set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
-    DESC
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-
-  end
 end
 
