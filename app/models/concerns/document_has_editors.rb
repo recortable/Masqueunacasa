ba164beb9da020d147177163f76b7a2d461d3afd
danigb <danigb@gmail.com>
2013-02-18 23:18:29 +0100

    Add editor if present

--- app/models/concerns/document_has_editors.rb
+++ app/models/concerns/document_has_editors.rb
@@ -7,7 +7,9 @@ module DocumentHasEditors
 
   protected
   def add_current_user_as_editor
-    self.document.add_editor(User.current_user) if self.document.respond_to?(:add_editor)
+    if User.current_user.present? && self.document.respond_to?(:add_editor)
+      self.document.add_editor(User.current_user) 
+    end
   end
 
 end
