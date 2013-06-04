4d6060a1ee3d0f113568f91890014b83b03fae50
Pau Compte <pausandalio@gmail.com>
2013-06-04 17:01:31 +0200

    Empezando a preparar servidor de transición

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ load "config/recipes/newrelic"
 load "config/recipes/log"
 load "config/recipes/apt"
 
-# server "176.58.98.122", :web, :app, :db, primary: true
+server "82.196.9.137", :web, :app, :db, primary: true
 
 set :application, "Masqueunacasa"
 set :user, "deployer"
@@ -24,13 +24,13 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:recortable/#{application}.git"
-set :branch, "master"
+set :branch, "transicion"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 # nginx stuff
-set :server_names, '<%= nginx_server_names %>'
+set :server_names, 'MQ1Ctransition'
 set :nginx_page_caching, true
 
 # newrelic settings
@@ -39,6 +39,9 @@ set :newrelic_license_key, '0a0d3776322392d64886579d8e72499290edd79a'
 # config files settings
 set :config_files, ['amazon_s3.yml', 'smtp.yml']
 
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
