48df2669c6c169bdcdf3fc2814d5d8276b76e013
Pau Compte <pausandalio@gmail.com>
2013-07-02 17:24:34 +0200

    Todas las traducciones de los textos de ayuda en el mismo formulario

--- app/models/concerns/has_translations.rb
+++ app/models/concerns/has_translations.rb
@@ -13,7 +13,6 @@ module HasTranslations
   protected
 
   def set_original_locale
-    self.original_locale = I18n.locale
+    self.original_locale = I18n.locale if respond_to(:original_locale)
   end
 end
-
--- app/models/help_text.rb
+++ app/models/help_text.rb
@@ -2,8 +2,22 @@ class HelpText < ActiveRecord::Base
   attr_accessible :identf, :description, :body, :blocked
   translates :body
 
+  include HasTranslations
+
   validates :identf, presence: true, format: { with: /\A\w+\z/ },
     length: { maximum: 30 }
   validates :description, length: { maximum: 300 }
-  validates :body, presence: true
+  validates :body_es, presence: true
+
+  T.avl.each do |l|
+    attr_accessible :"body_#{l.to_s}"
+
+    define_method "body_#{l.to_s}" do
+      T.w_l(l) { body if translated_to?(l) }
+    end
+
+    define_method "body_#{l.to_s}=" do |value|
+      T.w_l(l) { self.body = value if value.present? }
+    end
+  end
 end
