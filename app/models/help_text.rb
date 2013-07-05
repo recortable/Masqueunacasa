f5d06be607e2a73a744c989cfcf2a147754499b9
Pau Compte <pausandalio@gmail.com>
2013-07-05 14:06:55 +0200

    Solucionar problemas con los atributos virtuales y las validaciones de los textos de ayuda

--- app/models/help_text.rb
+++ app/models/help_text.rb
@@ -7,17 +7,23 @@ class HelpText < ActiveRecord::Base
   validates :identf, presence: true, format: { with: /\A\w+\z/ },
     length: { maximum: 30 }
   validates :description, length: { maximum: 300 }
-  validates :body_es, presence: true
+  validates :"body_#{T.dfl}", presence: true
+
+  after_initialize :set_translated_bodies
 
   T.avl.each do |l|
     attr_accessible :"body_#{l.to_s}"
 
-    define_method "body_#{l.to_s}" do
-      T.w_l(l) { body if translated_to?(l) }
-    end
-
     define_method "body_#{l.to_s}=" do |value|
       T.w_l(l) { self.body = value if value.present? }
+    end
+  end
+
+  private
+
+  def set_translated_bodies
+    T.avl.each do |l|
+      write_attribute "body_#{l}", translated_to?(l) ? T.w_l(l) { body } : nil
     end
   end
 end
