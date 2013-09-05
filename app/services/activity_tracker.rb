baef8117045ba53ca8c18b8e26e939e7a6452433
Pau Compte <pausandalio@gmail.com>
2013-09-05 15:21:13 +0200

    Refactorizar app/services/activity_tracker.rb

--- app/services/activity_tracker.rb
+++ app/services/activity_tracker.rb
@@ -1,37 +1,24 @@
 class ActivityTracker
   def initialize(trackable, owner, action)
-    @trackable = trackable
-    @owner = owner
-    @new_translation = action == :update &&
-      trackable.respond_to?('new_translation') &&
-      trackable.new_translation == "true"
-    @key = @new_translation ? :translate : action
+    @activity = PublicActivity::Activity.new
+    @activity.trackable = trackable
+    @activity.owner = owner
+    @activity.key = set_activity_key(action)
+    @activity.parameters = parameters
   end
 
   def track
-    @key = :translate if @new_translation
-    if @key == :destroy
-      track_destroy
-    else
-      @trackable.create_activity @key, owner: @owner, params: parameters
-    end
+    @activity.save
   end
 
   private
 
-  def parameters
-    { locale: T.l.to_s, attributes: @trackable.attributes }
+  def set_activity_key(action)
+    action = :translate if (action == :update && @activity.trackable.new_translation)
+    "#{@activity.trackable.class.name.downcase}.#{action.to_s}"
   end
 
-  def track_destroy
-    activity = PublicActivity::Activity.new
-    activity.owner = @owner
-    activity.assign_attributes({
-      key: "#{@trackable.class.name.downcase}.destroy",
-      trackable_id: @trackable.id,
-      trackable_type: @trackable.class.name,
-      parameters: parameters
-    }, without_protection: true)
-    activity.save
+  def parameters
+    { locale: T.l.to_s, attributes: @activity.trackable.changes }
   end
 end
