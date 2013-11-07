9ab1a594e44dea3ccabb845dd269a22217e891a4
Pau Compte <pausandalio@gmail.com>
2013-11-07 20:00:04 +0100

    Add excerpts and highlight search terms on search results

--- app/models/search.rb
+++ app/models/search.rb
@@ -20,16 +20,17 @@ class Search < ActiveRecord::Base
   belongs_to :searchable, polymorphic: true
 
   def results
-    self.class.global_search(Search.fts_dictionary(T.l), @query).
-      where(locale: T.l).preload(:searchable).map!(&:searchable).uniq
+    searches_rows = self.class.global_search(fts_dictionary, @query).
+      where(locale: T.l)
+    prepare_results(searches_rows)
   end
 
   def self.primary_key
     'searchable_id'
   end
 
-  def self.fts_dictionary(locale)
-    case locale.to_sym
+  def fts_dictionary
+    @dictionary ||= case T.l
     when :es
       'spanish'
     when :en
@@ -41,6 +42,57 @@ class Search < ActiveRecord::Base
     else
       'simple'
     end
+  end
+
+  private
+
+  def prepare_results(searches_rows)
+    searchables = searches_rows.preload(:searchable).map!(&:searchable).uniq.compact
+    searchables.each do |searchable|
+      searchable.title = highlight_result(searchable.title)
+      add_excerpts(searchable)
+    end
+  end
+
+  def add_excerpts(searchable)
+    excerpts = []
+    excerpts.push highlight_result(searchable.summary) if highlight_result?(searchable.summary)
+    if searchable.respond_to?(:sections)
+      searchable.sections.each do |section|
+        excerpts.push highlight_result(section.title) if highlight_result?(section.title)
+        excerpts.push highlight_result(section.body) if highlight_result?(section.body)
+      end
+    end
+    searchable["excerpts"] = excerpts.join(' ||| ')
+  end
+
+  def highlight_result?(string)
+    sql = "SELECT
+             to_tsvector('#{fts_dictionary}', unaccent(#{sanitized_string(string)}))
+           @@
+             plainto_tsquery('#{fts_dictionary}', unaccent(#{sanitized_query})) AS true_or_false"
+
+    query_result = ActiveRecord::Base.connection.execute(sql)
+    ActiveRecord::ConnectionAdapters::Column.value_to_boolean(query_result.first["true_or_false"])
+  end
+
+  def highlight_result(string)
+    sql = "SELECT
+             ts_headline(
+               '#{fts_dictionary}',
+               unaccent(#{sanitized_string(string)}),
+               plainto_tsquery('#{fts_dictionary}', unaccent(#{sanitized_query})),
+               'FragmentDelimiter = \" ||| \", StartSel = \"<span class=ts-highlight>\", StopSel = </span>'
+             )"
+    ActiveRecord::Base.connection.execute(sql).first["ts_headline"]
+  end
+
+  def sanitized_query
+    @sanitized_query ||= ActiveRecord::Base.sanitize(@query)
+  end
+
+  def sanitized_string(string)
+    ActiveRecord::Base.sanitize(string)
   end
 end
 
